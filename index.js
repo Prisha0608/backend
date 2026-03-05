53372f13ca82a7e0fc4b5e5b53d05de6fd19f0a5
Prisha0608 <prishakakkar27@gmail.com>
2026-03-05 22:03:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// console.log("chai aur code");
+require('dotenv').config()
+const express=require('express') // require module syntax with object express 
+const app=express() //using express you make variable app like math 
+const port=3000
+// / is home route
+// app listen at / home route and callback whenever request is send
+app.get('/',(req,res)=>{
+    res.send('hello world   gssd')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('whatsup')
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`example app listening on port ${port}`)
+})
